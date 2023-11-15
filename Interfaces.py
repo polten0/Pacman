5980e7dfe2d226c956baea7ecc84d029d2bc34bd
polten0 <alexey.mikryukov@mail.ru>
2023-11-16 00:21:27 +0300

    Interfaces, Objects

---
+++ Interfaces.py
@@ -0,0 +1,21 @@
+class IDrawableObject:
+    def __init__(self):
+        pass
+
+    def draw(self):
+        pass
+
+class IUpdateableObject:
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+
+class ITextureableObject:
+    def __init__(self):
+        pass
+
+    def loadContent(self):
+        pass
---
+++ Objects.py
@@ -0,0 +1,48 @@
+import Interfaces
+import vec
+import pyray
+class GameObject(Interfaces.IUpdateableObject,
+                 Interfaces.IDrawableObject):
+    def __init__(self):
+        self.vectorPosition = vec.Vector2(x=0, y=0)
+
+    @property
+    def X(self):
+        return self.vectorPosition.x
+    @X.setter
+    def X(self, new):
+        self.vectorPosition.x = new
+
+    @property
+    def Y(self):
+        return self.vectorPosition.y
+    @Y.setter
+    def Y(self, new):
+        self.vectorPosition.y = new
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
+
+class MapObject(Interfaces.IDrawableObject):
+    def __init__(self):
+        self.vectorPosition = vec.Vector2(x=0, y=0)
+
+    @property
+    def X(self):
+        return self.vectorPosition.x
+    @X.setter
+    def X(self, new):
+        self.vectorPosition.x = new
+
+    @property
+    def Y(self):
+        return self.vectorPosition.y
+    @Y.setter
+    def Y(self, new):
+        self.vectorPosition.y = new
+
+    def draw(self):
+        pass
