cf4f053c2017bb7ac71f24b6f5adb4a191972f45
polten <alexey.mikryukov@mail.ru>
2023-11-16 19:39:08 +0300

    fix

--- AppCore/Managers.py
+++ AppCore/Managers.py
@@ -2,10 +2,12 @@ import pyray
 from ObjectClasses.Objects import GameObject, MapObject, UIObject
 
 class AppManager:
+    instanse = None
     screenWidth = 500
     screenHeight = 700
     def __init__(self):
         self.gameManager = GameManager()
+        AppManager.instanse = self
 
     def Initialization(self):
         pyray.init_window(AppManager.screenWidth, AppManager.screenHeight, 'Game')
@@ -24,12 +26,14 @@ class AppManager:
 class MapManager:
     def __init__(self):
         pass
+
     def Draw(self):
         pass
 
 class GameManager:
     def __init__(self):
         self.listGameObjects = list([GameObject()])
+
 
     def Update(self):
         for gameObject in self.listGameObjects:
@@ -38,6 +42,11 @@ class GameManager:
     def Draw(self):
         for gameObject in self.listGameObjects:
             gameObject.draw()
+
+    def CheckCollision(self):
+        for gameObject in self.listGameObjects:
+            gameObject.update()
+
 
 class GUIManager:
     def __init__(self):
