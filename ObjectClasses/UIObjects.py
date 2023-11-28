13c2d4305a6557697296b64946dcd8551cafd5e7
owfoe <gvinnik07@gmail.com>
2023-11-28 17:34:52 +0300

    рабочий текст!!

--- ObjectClasses/UIObjects.py
+++ ObjectClasses/UIObjects.py
@@ -6,16 +6,21 @@ from ObjectClasses.Objects import UIObject
 class Label(UIObject):
     def __init__(self, x = 0, y = 0, text="null"):
         super().__init__()
-        self.Font = pyray.load_font(f"{os.getcwd()}/Content/Font/joystix monospace.ttf")
         self.Text = text
-        self.FontSize = 10
+        self.Font = None
+        self.fontPath = ""
+        self.FontSize = 40
         self.Spacing = 1
-        self.X(x)
-        self.Y(y)
+        self.X = x
+        self.Y = y
+
 
     def draw(self):
-        pyray.draw_text_ex(self.Font, self.Text, self.X, self.Y, self.FontSize, self.Spacing, pyray.WHITE)
+        pyray.draw_text_ex(self.Font, self.Text, pyray.Vector2(self.X, self.Y), self.FontSize, self.Spacing, pyray.WHITE)
 
     def update(self, new_text):
         self.Text = new_text
-        pyray.draw_text_ex(self.Font, self.Text, self.X, self.Y, self.FontSize, self.Spacing, pyray.WHITE)
+
+    def loadFont(self):
+        self.fontPath = f"{os.getcwd()}/Content/Font/joystix monospace.ttf"
+        self.Font = pyray.load_font(self.fontPath)
